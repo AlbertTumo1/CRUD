3b6b1c047eda10bce95a15e3a31ea8d517ef3b9c
AlbertTumo1 <albert.z.y@tumo.org>
2024-01-25 21:22:14 +0400

    CRUD Operations and Frontend Work

--- public/script.js
+++ public/script.js
@@ -1,15 +1,16 @@
 const name = document.getElementById("name");
 const age = document.getElementById("age");
+const email = document.getElementById("email");
 const submitBtn = document.getElementById("submit");
 
 submitBtn.addEventListener("click", submitForm);
 
 function submitForm() {
-    fetch("http://localhost:3000/add-info", {
+    fetch("http://localhost:3000/add-user", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({name:name.value, age:age.value})        
+        body: JSON.stringify({name:name.value, age:age.value, email:email.value})        
     })
 }
--- server.js
+++ server.js
@@ -1,49 +1,44 @@
-// const express = require("express");
-// const bodyParser = require('body-parser');
-// const path = require("path");
-
-// const app = express();
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.use(express.static('public'));
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname,'./public/form.html'));
-// });
-
-// app.post('/add-info', (req, res) => {
-//     const { name, age } = req.body;
-//     console.log('Data:', name , age);
-//     res.redirect('/');
-// });
-
-// app.listen(3000, () => console.log("Example is running on port 3000"));
-
+const express = require("express");
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://AlbertTumo:xboxones@tumocrud.f6doipk.mongodb.net/sample_mflix';
+const bodyParser = require('body-parser');
+const path = require("path");
 
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+const connectionString = 'mongodb+srv://AlbertTumo:xboxones@tumocrud.f6doipk.mongodb.net/sample_mflix';
 mongoose.connect(connectionString);
 
-const sessionSchema = new mongoose.Schema({
-    full_name: String,
-    email_address:String,
-    city:String,
-    country:String
-});
-
 const db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'Connection error:'));
 
-db.once('open', async () => {
-    console.log('Connected to MongoDB!');
-
-    await mongoose.connection.db.collection("sessions").insertOne({name: "Albert"});
-
-    const sessions = await mongoose.connection.db.collection("sessions").find().toArray();
-
-    console.log(sessions)
-
-    mongoose.connection.close();
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/form.html'));
 });
+
+app.post('/add-user', async (req, res) => {
+    const { name, age, email } = req.body;
+    console.log('Data:', name , age);
+
+    db.once("open", async () => {
+        try {
+            console.log("Connected to MongoDB!");
+            const user = await mongoose.connection.db.collection("test_users").insertOne({ name, email, age });
+            res.json(user)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            mongoose.connection.close();
+            console.log("Closed Connection");
+        }
+    })
+
+    res.redirect('/');
+});
+
+app.listen(3000, () => console.log("Example is running on port 3000"));
